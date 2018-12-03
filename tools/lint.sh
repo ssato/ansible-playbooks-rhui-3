ab537daac63607a69584104841750136b458c871
Satoru SATOH <satoru.satoh@gmail.com>
2018-12-04 04:39:06 +0900

    fix: try ansible-lint-3 first

--- tools/lint.sh
+++ tools/lint.sh
@@ -4,11 +4,12 @@ set -ex
 
 playbooks=${1:-*.yml}
 inventory=inventory/hosts
+ansible_lint="$(which ansible-lint-3 2>/dev/null || which ansible-lint 2>/dev/null)"
 
 yamllint --strict $(find . -type f | grep -E '.yml$')
 
 for p in ${playbooks:?}; do
-        which ansible-lint 2>/dev/null && ansible-lint -vvv $p || ansible-lint-3 -vvv $p
+        ${ansible_lint:?} -vvv $p
         ansible-playbook -i ${inventory:?} -vvv $p --syntax-check
 done
 
